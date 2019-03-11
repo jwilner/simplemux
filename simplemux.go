08123f76b3a2484359c2cfbabfdaeb85c2a90eb5
Joe Wilner <jhwilner1@gmail.com>
2019-03-10 23:39:35 -0400

    var matching

--- simplemux.go
+++ simplemux.go
@@ -3,6 +3,7 @@ package simplemux
 import (
 	"context"
 	"net/http"
+	"regexp"
 	"strings"
 )
 
@@ -20,13 +21,10 @@ type Filter interface {
 type Middleware func(http.Handler) http.Handler
 
 func New() *Router {
-	engine := &engine{root: newNode()}
+	engine := &engine{root: newNode(nil)}
+	builder := RouteBuilder{engine: engine}
 
-	return &Router{
-		engine:         engine,
-		routeStarter:   RouteBuilder{engine: engine},
-		defaultHandler: http.NotFoundHandler(),
-	}
+	return &Router{engine: engine, routeStarter: builder, defaultHandler: http.NotFoundHandler()}
 }
 
 // defines the valid ways of beginning a route
@@ -57,7 +55,18 @@ func (r *Router) ServeHTTP(w http.ResponseWriter, req *http.Request) {
 			}
 
 			if !filtered {
-				h.ServeHTTP(w, SetVars(req, vars))
+				if len(vars) == 0 {
+					h.ServeHTTP(w, req)
+					return
+				}
+
+				// zip found vars and the handler's name for them
+				varMap := make(map[string]string, len(vars))
+				for i, val := range vars {
+					varMap[h.names[i]] = val
+				}
+
+				h.ServeHTTP(w, req.WithContext(context.WithValue(req.Context(), keyVars, varMap)))
 				return
 			}
 		}
@@ -72,10 +81,6 @@ const (
 	keyVars ctxKey = 1
 )
 
-func SetVars(r *http.Request, vars []string) *http.Request {
-	return r.WithContext(context.WithValue(r.Context(), keyVars, vars))
-}
-
 func Vars(r *http.Request) []string {
 	return r.Context().Value(keyVars).([]string)
 }
@@ -84,53 +89,154 @@ type engine struct {
 	root *node
 }
 
+var regexpVar = regexp.MustCompile(`^{([^:}]+)(?::([^}/]+))?}/?$`)
+
 func (e *engine) insert(r route) {
 	node := e.root
+
+	var varNames []string
 	for _, segment := range strings.SplitAfter(r.path, "/") {
-		if _, ok := node.children[segment]; !ok {
-			node.children[segment] = newNode()
+		if groups := regexpVar.FindStringSubmatch(segment); len(groups) > 0 {
+			varNames = append(varNames, groups[1])
+			trailingSlash := segment[len(segment)-1] == '/'
+			if len(groups) == 3 {
+				node = node.addChild(&regexpMatcher{
+					reg:   regexp.MustCompile(groups[2]),
+					slash: trailingSlash,
+				})
+			} else {
+				node = node.addChild(wildCardMatcher(trailingSlash))
+			}
+		} else {
+			node = node.addChild(stringMatcher(segment))
 		}
-		node = node.children[segment]
 	}
 
 	node.handlers = append(node.handlers, handler{
 		Handler: r.handler,
 		method:  r.method,
 		filters: r.filters,
+		names:   varNames,
 	})
 }
 
-func newNode() *node {
-	return &node{
-		children: make(map[string]*node),
-	}
+func newNode(m matcher) *node {
+	return &node{matcher: m}
 }
 
 type node struct {
-	children map[string]*node
+	matcher
+	children []*node
 	handlers []handler
+}
+
+func (n *node) addChild(m matcher) *node {
+	insertPos := len(n.children)
+
+newMSwitch:
+	switch newM := m.(type) {
+	case stringMatcher:
+		for i, child := range n.children {
+			switch old := child.matcher.(type) {
+			case stringMatcher:
+				if newM == old {
+					return child // no need for insert
+				}
+			case *regexpMatcher, wildCardMatcher:
+				insertPos = i
+				break newMSwitch
+			}
+		}
+	case *regexpMatcher:
+		for i, child := range n.children {
+			switch old := child.matcher.(type) {
+			case stringMatcher:
+				// implicit continue
+			case *regexpMatcher:
+				if newM.reg.String() == old.reg.String() && newM.slash == old.slash {
+					return child
+				}
+			case wildCardMatcher:
+				insertPos = i
+				break newMSwitch
+			}
+		}
+	case wildCardMatcher:
+		for _, child := range n.children {
+			switch old := child.matcher.(type) {
+			case stringMatcher, *regexpMatcher:
+				// implicit continue
+			case wildCardMatcher:
+				if newM == old {
+					return child
+				}
+			}
+		}
+	}
+
+	child := newNode(m)
+
+	children := make([]*node, len(n.children)+1, len(n.children)+1)
+	copy(children, n.children[:insertPos])
+	children[insertPos] = child
+	copy(children[insertPos+1:], n.children[insertPos:])
+
+	n.children = children
+
+	return child
+}
+
+type matcher interface {
+	match(s string) (string, bool)
+}
+
+type regexpMatcher struct {
+	reg   *regexp.Regexp
+	slash bool
+}
+
+func (r *regexpMatcher) match(s string) (string, bool) {
+	if len(s) > 0 && (s[len(s)-1] == '/') != r.slash {
+		return "", false
+	}
+	return "", r.reg.MatchString(s)
+}
+
+type stringMatcher string
+
+func (sm stringMatcher) match(s string) (string, bool) {
+	return "", s == string(sm)
+}
+
+type wildCardMatcher bool
+
+func (wc wildCardMatcher) match(s string) (string, bool) {
+	if l := len(s) - 1; l >= 0 && s[l] == '/' {
+		if wc {
+			return s[:l], true
+		}
+	} else if !wc {
+		return s, false
+	}
+	return "", false
 }
 
 func (e *engine) match(path string) ([]string, []handler) {
 	var vars []string
 	node := e.root
-	for _, v := range strings.SplitAfter(path, "/") {
-		node := node.children[v]
-		if node == nil && len(v) > 0 {
-			if v[len(v)-1] == '/' {
-				if node = node.children["*/"]; node != nil {
-					vars = append(vars, v[:len(v)-1])
+
+	for _, segment := range strings.SplitAfter(path, "/") {
+		for _, child := range node.children {
+			if pathVar, ok := child.match(segment); ok {
+				if pathVar != "" {
+					vars = append(vars, pathVar)
 				}
-			} else {
-				if node = node.children["*"]; node != nil {
-					vars = append(vars, v)
-				}
+				node = child
 			}
 		}
-		if node == nil {
-			return nil, nil
-		}
+		return nil, nil
 	}
+
 	return vars, node.handlers
 }
 
@@ -138,6 +244,7 @@ type handler struct {
 	http.Handler
 	method  string
 	filters []Filter
+	names   []string
 }
 
 type route struct {
